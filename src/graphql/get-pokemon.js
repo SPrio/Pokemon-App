daf6613102ed910c58f48ea3ee7eb910a46dcc2e
supriyo-peopletech <supriyo.mahanta@peopletech.com>
2020-12-20 17:10:34 +0530

    updated graphql and deisgned
    Author:    SPrio <mahantasupriyo@gmail.com>
    Date:      Sun Dec 20 17:10:34 2020 +0530

---
+++ src/graphql/get-pokemon.js
@@ -0,0 +1,20 @@
+import { gql } from "apollo-boost";
+
+export const GET_POKEMONS = gql`
+  query pokemons($first: Int!) {
+    pokemons(first: $first) {
+      id
+      name
+      image
+      maxHP
+      maxCP
+      attacks {
+        special {
+          name
+          type
+          damage
+        }
+      }
+    }
+  }
+`;
